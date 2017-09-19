d2dc9b13f7869a88a00cebc8f0a635d203a08325
David Hodge <david.hodge@lucidtechnics.com>
2017-09-19 19:43:55 +0000

    rebuild pages at 1d24c16

--- generated/scripts/main.js
+++ generated/scripts/main.js
@@ -91,6 +91,65 @@
     /* enable tooltips */
     $('[data-toggle="tooltip"]').tooltip({container: 'body', delay: { show: 200, hide: 0 } });
 
+
+    //adding ellipsis to the large text
+    var ellipsisText = function(e) {
+        //complete text stored in a variable
+        var completeText = e.innerHTML.split(' ');
+        //wordArray will be the text displayed
+        var wordArray = e.innerHTML.split(' ');
+        var lineNumMax = e.className.match(/\d/g);
+        var finalHeight = lineNumMax * 18; //18 is the  line height
+
+        while (e.scrollHeight > finalHeight) {
+            wordArray.pop();
+
+            e.innerHTML = wordArray.join(' ') + '<span class="ellipsis">... </span>';
+
+
+        }
+
+        //save the additional text in the span
+        e.innerHTML = e.innerHTML + ' <span class="additional-text">' + completeText.splice(wordArray.length).join(' ') + '</span>';
+    };
+    // this should be a function which you will call on load
+    function addEllipsis(el) {
+        [].forEach.call(el, function(elem) {
+            ellipsisText(elem);
+        });
+    }
+
+    //add ellipsis to all p using the class
+    var addEllipsisSelector = document.querySelectorAll('.text-ellipsis-4line');
+    addEllipsis(addEllipsisSelector);
+    var removeEllipsisSelector = document.querySelectorAll('.mdl-card-expand');
+    //expand the text when this function is called    
+    for (var i = 0; i < removeEllipsisSelector.length; i++) {
+        //add mouse enter or hover event listener on the div
+        removeEllipsisSelector[i].addEventListener('mouseenter', function() {
+            var paraText = this.querySelectorAll('p[class*="text-ellipsis-"]')[0];
+            var listText = this.querySelectorAll('ul[class*="show-list-"]')[0];
+            if (typeof(paraText) !== 'undefined' && paraText !== null) {
+            paraText.classList.add('expanded-view');
+            }
+            if (typeof(listText) !== 'undefined' && listText !== null) {
+            listText.classList.add('expanded-view');
+            }
+
+        });
+        //add key enter or focus event listener on the div
+        removeEllipsisSelector[i].addEventListener('focus', function() {
+            var paraText = this.querySelectorAll('p[class*="text-ellipsis-"]')[0];
+            var listText = this.querySelectorAll('ul[class*="show-list-"]')[0];
+            if (typeof(paraText) !== 'undefined' && paraText !== null) {
+            paraText.classList.add('expanded-view');
+            }
+            if (typeof(listText) !== 'undefined' && listText !== null) {
+            listText.classList.add('expanded-view');
+            }
+        });
+    }
+
     // enable datepickers (NOTE the 'not' method to avoid double init of range examples!)
     $('.datepicker').not('#startRange').not('#endRange').datepicker({
         autoSize: true,
