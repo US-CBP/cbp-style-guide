039bec7212ba774a74a413fb9ee8cbe63f75f681
Menka Kayan <mkayan@users.noreply.github.com>
2017-10-04 10:02:28 -0400

    Table update (#65)
    * updating tables layout based on the new theme update
    
    * updating change log
    
    * moving to jquery 2

--- src/front/scripts/main.js
+++ src/front/scripts/main.js
@@ -1,17 +1,73 @@
 'use strict';
-(function () {
+(function() {
+    //layout example js
+    $(window).on('load', function() {
+        //make the header sticky
+        $(window).scrollTop(0);
+        $(window).resize(function() {
 
+            //height of only top header
+            var sidebarOffsetTopHeader = $('.cbp-header').height();
+
+            //height of only app header
+            var sidebarOffsetSingleHeader = $('.app-header').height();
+
+            //height of both header
+            var sidebarOffsetDoubleHeader = sidebarOffsetTopHeader + sidebarOffsetSingleHeader;
+
+            //dynamically add the offset to sidebar on page load
+            if ($('.app-header').length === 0) {
+                $('.sidebar').css('top', sidebarOffsetTopHeader);
+            } else {
+                $('.sidebar').css('top', sidebarOffsetDoubleHeader);
+            }
+
+
+            //scroll function 
+            $(window).scroll(function() {
+
+                //check when the scroll is more than the height of universal header
+                var scroll = $(window).scrollTop();
+
+                //check if the app header is there or not
+                if ($('.app-header').length === 0) {
+                    //check when the scroll is more than the height of universal header
+                    if (scroll >= sidebarOffsetTopHeader) {
+                        $('.sidebar').css('top', 0);
+                    } else {
+                        $('.sidebar').css('top', sidebarOffsetTopHeader);
+                    }
+                } else {
+                    //check when the scroll is more than the height of universal header
+                    if (scroll >= sidebarOffsetTopHeader) {
+                        $('.app-header').addClass('top');
+                        $('.sidebar').css('top', sidebarOffsetSingleHeader);
+                    } else {
+                        $('.app-header').removeClass('top');
+                        $('.sidebar').css('top', sidebarOffsetDoubleHeader);
+                    }
+                }
+
+
+            });
+
+            $('.content').css('padding-top', sidebarOffsetSingleHeader);
+
+
+        }).resize();
+
+    });
     /*
      * Add toggle code functionality
      */
-    $('.pl-preview').each(function () {
+    $('.pl-preview').each(function() {
         var el = $(this);
         var next = el.next('.highlight');
         var toggle;
         if (next.length) {
             toggle = $('<div class="pl-toggle-code"><i class="icon icon-code"></i> View source</div>');
             toggle.prependTo(el);
-            toggle.on('click', function () {
+            toggle.on('click', function() {
                 next.slideToggle(250);
                 toggle.toggleClass('active');
             });
@@ -23,10 +79,10 @@
      */
     var activeSubNav = $('.pl-sidebar > .nav > .active');
     var newList = $('<ul class="nav nav-stacked"></ul>');
-    $('.pl-pattern > h3').each(function () {
+    $('.pl-pattern > h3').each(function() {
         var el = $(this);
         if (el.attr('id')) {
-            var li = $('<li><a href="#'+ el.attr('id') +'">'+ el.text() +'</a></li>');
+            var li = $('<li><a href="#' + el.attr('id') + '">' + el.text() + '</a></li>');
             newList.append(li);
 
         }
@@ -36,13 +92,13 @@
     }
 
     var autoCollapsed = false;
-    $('.nav-collapse').on('click', function () {
+    $('.nav-collapse').on('click', function() {
         autoCollapsed = false;
         $('body').toggleClass('pl-collapsed-nav');
     });
 
     /* collapse sidebar when necessary */
-    var checkNav = function () {
+    var checkNav = function() {
         var collapsed = $('body').hasClass('pl-collapsed-nav'),
             width = $(this).width();
 
@@ -65,7 +121,7 @@
     $('body').scrollspy({ target: '.pl-sidebar > .nav > .active', offset: 120 });
 
     /* animate scrolling to the sidebar sublink targets to ensure proper offsets */
-    $('.pl-sidebar > .nav > .active > .nav > li > a').on('click', function () {
+    $('.pl-sidebar > .nav > .active > .nav > li > a').on('click', function() {
         $('html, body').animate({
             scrollTop: $($(this).attr('href')).offset().top - 110
         }, 200);
@@ -89,7 +145,7 @@
     $('.selectize-multiple').selectize({ allowClear: true });
 
     /* enable tooltips */
-    $('[data-toggle="tooltip"]').tooltip({container: 'body', delay: { show: 200, hide: 0 } });
+    $('[data-toggle="tooltip"]').tooltip({ container: 'body', delay: { show: 200, hide: 0 } });
 
 
     //adding ellipsis to the large text
@@ -130,10 +186,10 @@
             var paraText = this.querySelectorAll('p[class*="text-ellipsis-"]')[0];
             var listText = this.querySelectorAll('ul[class*="show-list-"]')[0];
             if (typeof(paraText) !== 'undefined' && paraText !== null) {
-            paraText.classList.add('expanded-view');
+                paraText.classList.add('expanded-view');
             }
             if (typeof(listText) !== 'undefined' && listText !== null) {
-            listText.classList.add('expanded-view');
+                listText.classList.add('expanded-view');
             }
 
         });
@@ -142,10 +198,10 @@
             var paraText = this.querySelectorAll('p[class*="text-ellipsis-"]')[0];
             var listText = this.querySelectorAll('ul[class*="show-list-"]')[0];
             if (typeof(paraText) !== 'undefined' && paraText !== null) {
-            paraText.classList.add('expanded-view');
+                paraText.classList.add('expanded-view');
             }
             if (typeof(listText) !== 'undefined' && listText !== null) {
-            listText.classList.add('expanded-view');
+                listText.classList.add('expanded-view');
             }
         });
     }
@@ -158,7 +214,7 @@
         showOtherMonths: true,
         changeMonth: true,
         changeYear: true,
-        onClose : function() {
+        onClose: function() {
             // needed for mdl textfield floading labels!
             if ($(this).parent().get(0).MaterialTextfield) {
                 $(this).parent().get(0).MaterialTextfield.checkDirty();
@@ -173,10 +229,10 @@
         showOtherMonths: true,
         changeMonth: true,
         changeYear: true,
-        onSelect : function(dateStr) {
-            $('#endRange.datepicker').datepicker( 'option', 'minDate', dateStr);
+        onSelect: function(dateStr) {
+            $('#endRange.datepicker').datepicker('option', 'minDate', dateStr);
         },
-        onClose : function() {
+        onClose: function() {
             // needed for mdl textfield floading labels!
             if ($(this).parent().get(0).MaterialTextfield) {
                 $(this).parent().get(0).MaterialTextfield.checkDirty();
@@ -190,10 +246,10 @@
         showOtherMonths: true,
         changeMonth: true,
         changeYear: true,
-        onSelect : function(dateStr) {
-            $('#startRange.datepicker').datepicker( 'option', 'maxDate', dateStr);
+        onSelect: function(dateStr) {
+            $('#startRange.datepicker').datepicker('option', 'maxDate', dateStr);
         },
-        onClose : function() {
+        onClose: function() {
             // needed for mdl textfield floading labels!
             if ($(this).parent().get(0).MaterialTextfield) {
                 $(this).parent().get(0).MaterialTextfield.checkDirty();
@@ -202,25 +258,25 @@
     });
 
     /* find checkboxes with the indeterminate attr and set the indeterminate property */
-    $('input[type="checkbox"][indeterminate]').each(function () {
+    $('input[type="checkbox"][indeterminate]').each(function() {
         this.indeterminate = true;
     });
 
 
     /* closable toast examples */
-    $('.pl-closable-toasts .toast .close').each(function () {
-        $(this).on('click', function () {
+    $('.pl-closable-toasts .toast .close').each(function() {
+        $(this).on('click', function() {
             $(this).closest('.toast').animate({
                 right: '-100%',
                 opacity: 0
-            }, 150, 'swing', function () {
+            }, 150, 'swing', function() {
                 $(this).hide(150);
             });
         });
     });
 
     /* quick and dirty working toast examples */
-    $('.show-toasts').each(function () {
+    $('.show-toasts').each(function() {
         var showToastButton = $(this);
 
         // get toasts and move to body
@@ -232,25 +288,25 @@
         var closers = toastContainer.find('.close');
         var closersCount = closers.length;
         var closedCount = 0;
-        closers.each(function () {
-            $(this).on('click', function () {
+        closers.each(function() {
+            $(this).on('click', function() {
                 if (closersCount === ++closedCount) {
                     closedCount = 0;
-                    setTimeout(function () {
+                    setTimeout(function() {
                         toastContainer.hide();
                     }, 150);
                 }
             });
         });
 
-        var showToast = function (toastEl) {
-            toastEl.css({right: '-100%', opacity: 0, display: 'block'});
+        var showToast = function(toastEl) {
+            toastEl.css({ right: '-100%', opacity: 0, display: 'block' });
             toastEl.animate({
                 right: '0',
                 opacity: 1
-            }, 150, 'swing', function () {
+            }, 150, 'swing', function() {
                 if (toastEl.next().length) {
-                    setTimeout(function () {
+                    setTimeout(function() {
                         showToast(toastEl.next());
                     }, 1000);
                 }
@@ -259,7 +315,7 @@
 
         // show the toasts on click
         showToastButton.on('click', function() {
-            toastContainer.find('.toast').hide().css({right: 0, opacity: 1});
+            toastContainer.find('.toast').hide().css({ right: 0, opacity: 1 });
             toastContainer.toggle();
             if (toastContainer.css('display') === 'block') {
                 toastContainer.css({
@@ -274,11 +330,11 @@
         });
     });
 
-    $('.nav-stacked.nav-tree > li').each(function () {
+    $('.nav-stacked.nav-tree > li').each(function() {
         var self = $(this);
         var nested = $(this).find('> .nav-stacked');
         var expander = $(this).find('> .expander');
-        expander.on('click', function (e) {
+        expander.on('click', function(e) {
             e.preventDefault();
             e.stopPropagation();
             var visible = nested.is(':visible');
@@ -309,7 +365,7 @@
         var barEl = $(this);
         var valueEl = $('#exampleProgressBarValue');
         var progress = parseInt(this.style.width, 10);
-        setInterval(function () {
+        setInterval(function() {
             barEl.css('width', ((progress += 19) % 100) + '%');
             valueEl.text(progress % 100 + '%');
         }, 1000);
@@ -341,7 +397,7 @@
 
 
     /* enable noui sliders */
-    $('[data-nouislider]').each(function () {
+    $('[data-nouislider]').each(function() {
         var el = $(this);
         var opts = {
             range: {
@@ -361,7 +417,7 @@
             opts.start = parseInt(el.attr('data-start'), 10);
         }
         if (el.attr('data-start-b')) {
-            opts.start = [parseInt(el.attr('data-start'), 10),parseInt(el.attr('data-start-b'), 10)] ;
+            opts.start = [parseInt(el.attr('data-start'), 10), parseInt(el.attr('data-start-b'), 10)];
             opts.connect = true;
             opts.behaviour = 'drag';
         }
@@ -380,7 +436,7 @@
         }
 
         if (el.attr('data-slider-tooltip')) {
-            el.Link('lower').to('-inline-<div class="tooltip fade in top" style="top: -39px; transform: translateX(-50%); margin-left: 6px; display: inline-block; position: relative; width: auto;"></div>', function ( value ) {
+            el.Link('lower').to('-inline-<div class="tooltip fade in top" style="top: -39px; transform: translateX(-50%); margin-left: 6px; display: inline-block; position: relative; width: auto;"></div>', function(value) {
                 $(this).html(
                     '<div class="tooltip-inner">' + parseInt(value, 10) + '</div>' +
                     '<div class="tooltip-arrow"></div>'
@@ -396,7 +452,7 @@
         }
     });
     /* expandable rows */
-    $('table[data-pl-expandable-rows] > tbody > tr').on('click', function () {
+    $('table[data-pl-expandable-rows] > tbody > tr').on('click', function() {
         var val = $(this).attr('aria-expanded') === 'true';
         $(this).attr('aria-expanded', !val);
         $(this).next('tr[data-pl-detail-row]').toggle();
@@ -406,42 +462,41 @@
     // Define the tour!
     /* jshint ignore:start */
     var tour = {
-      id: 'hopscotch-example',
-      steps: [
-        {
-          title: 'Hopscotch Tour Example',
-          content: 'This is the header of my page.',
-          target: 'tour',
-          placement: 'top'
-        },
-        {
-          title: 'Purpose',
-          content: 'Example of tour.',
-          target: 'purpose',
-          placement: 'bottom'
-        }, {
-          title: 'Navigation',
-          content: 'Foundation, Components, Patterns describe how best to use the CBP Theme.',
-          target: 'sidebar',
-          placement: 'right'
-        }
+        id: 'hopscotch-example',
+        steps: [{
+                title: 'Hopscotch Tour Example',
+                content: 'This is the header of my page.',
+                target: 'tour',
+                placement: 'top'
+            },
+            {
+                title: 'Purpose',
+                content: 'Example of tour.',
+                target: 'purpose',
+                placement: 'bottom'
+            }, {
+                title: 'Navigation',
+                content: 'Foundation, Components, Patterns describe how best to use the CBP Theme.',
+                target: 'sidebar',
+                placement: 'right'
+            }
 
-      ],
-      showPrevButton: true
+        ],
+        showPrevButton: true
     };
 
     // Start the tour!
-    if(document.URL.indexOf('tours.html') > -1 ){
+    if (document.URL.indexOf('tours.html') > -1) {
         hopscotch.startTour(tour);
     }
 
     var calloutMgr = hopscotch.getCalloutManager();
     calloutMgr.createCallout({
-      id: 'attach-icon',
-      target: 'callout-button',
-      placement: 'right',
-      title: 'Callout Example',
-      content: 'For simple explanations.'
+        id: 'attach-icon',
+        target: 'callout-button',
+        placement: 'right',
+        title: 'Callout Example',
+        content: 'For simple explanations.'
     });
     /* jshint ignore:end */
     /* google analytics download tracking */
@@ -468,7 +523,7 @@
         ext += ',rar,7z,exe,wma,mov,avi,wmv,mp3,csv,tsv';
         ext = ext.split(',');
 
-        $('a').on('mousedown', function () {
+        $('a').on('mousedown', function() {
             var el = this;
             if (el.href) {
                 var match = _checkFile.call(null, el.href, ext);
