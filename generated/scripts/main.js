a307c3e23c33056af783e95bbd10317f8caaa250
David Hodge <david.hodge@lucidtechnics.com>
2018-11-08 17:16:37 +0000

    rebuild pages at 4aed1d2

--- generated/scripts/main.js
+++ generated/scripts/main.js
@@ -16,19 +16,19 @@ $(document).ready(function () {
     delay: {show: 200, hide: 0}
   });
 
-      // enable popover
+  // enable popover
   $('[data-toggle="popover"]').popover();
 
-      // enable masks
+  // enable masks
   $('[data-inputmask]').inputmask();
 
-      // TOC
+  // TOC
   $('.toc a').on('click', function () {
     $('.toc a.active').removeClass('active');
     $(this).addClass('active');
   });
 
-      // progress bar
+  // progress bar
   $('#exampleProgressBar').each(function () {
     var barEl = $(this);
     var valueEl = $('#exampleProgressBarValue');
@@ -39,11 +39,9 @@ $(document).ready(function () {
     }, 1000);
   });
 
-      //
-      // Init datepicker
-      //
 
-      // enable datepickers (NOTE the 'not' method to avoid double init of range examples!)
+  // Init datepicker
+  // enable datepickers (NOTE the 'not' method to avoid double init of range examples!)
   $('.datepicker').not('#startRange').not('#endRange').datepicker({
     autoSize: true,
     showButtonPanel: true,
@@ -52,14 +50,80 @@ $(document).ready(function () {
     changeMonth: true,
     changeYear: true,
     onClose: function () {
-          // needed for mdl textfield floading labels!
+      // needed for mdl textfield floading labels!
       if ($(this).parent().get(0).MaterialTextfield) {
         $(this).parent().get(0).MaterialTextfield.checkDirty();
       }
     }
   });
 
-      // specific to our date range example
+  //adding ellipsis to the large text
+  var ellipsisText = function(e) {
+    //complete text stored in a variable
+    var completeText = e.innerHTML.split(' ');
+    //wordArray will be the text displayed
+    var wordArray = e.innerHTML.split(' ');
+    var lineNumMax = e.className.match(/\d/g);
+    var finalHeight = lineNumMax * 18; //18 is the  line height
+
+    while (e.scrollHeight > finalHeight) {
+      wordArray.pop();
+      e.innerHTML = wordArray.join(' ') + '<span class="ellipsis">... </span>';
+    }
+
+    //save the additional text in the span
+    e.innerHTML = e.innerHTML + ' <span class="additional-text">' + completeText.splice(wordArray.length).join(' ') + '</span>';
+  };
+
+  // this should be a function which you will call on load
+  function addEllipsis(el) {
+    [].forEach.call(el, function(elem) {
+      ellipsisText(elem);
+    });
+  }
+
+  //add ellipsis to all p using the class
+  var addEllipsisSelector = document.querySelectorAll('.text-ellipsis-4line');
+  addEllipsis(addEllipsisSelector);
+  var removeEllipsisSelector = document.querySelectorAll('.mdl-card-expand');
+  //expand the text when this function is called    
+  for (var i = 0; i < removeEllipsisSelector.length; i++) {
+    //add mouse enter or hover event listener on the div
+    removeEllipsisSelector[i].addEventListener('mouseenter', function() {
+      var paraText = this.querySelectorAll('p[class*="text-ellipsis-"]')[0];
+      var listText = this.querySelectorAll('ul[class*="show-list-"]')[0];
+      if (typeof(paraText) !== 'undefined' && paraText !== null) {
+        paraText.classList.add('expanded-view');
+      }
+      if (typeof(listText) !== 'undefined' && listText !== null) {
+        listText.classList.add('expanded-view');
+      }
+    });
+
+    //add key enter or focus event listener on the div
+    removeEllipsisSelector[i].addEventListener('focus', function() {
+      var paraText = this.querySelectorAll('p[class*="text-ellipsis-"]')[0];
+      var listText = this.querySelectorAll('ul[class*="show-list-"]')[0];
+      if (typeof(paraText) !== 'undefined' && paraText !== null) {
+        paraText.classList.add('expanded-view');
+      }
+      if (typeof(listText) !== 'undefined' && listText !== null) {
+        listText.classList.add('expanded-view');
+      }
+    });
+  }
+
+  //brand card interaction
+  $('.btn-circle').on('click', function() {
+    if ($(this).parents('.mdl-card').hasClass('brand-card-expand')) {
+      $(this).parents('.mdl-card').removeClass('brand-card-expand');
+    } else {
+      $('.mdl-card').removeClass('brand-card-expand');
+      $(this).parents('.mdl-card').addClass('brand-card-expand');
+    }
+  });
+
+   // specific to our date range example
   $('#startRange.datepicker').datepicker({
     autoSize: true,
     showButtonPanel: true,
@@ -68,11 +132,10 @@ $(document).ready(function () {
     changeMonth: true,
     changeYear: true,
     onSelect: function (dateStr) {
-          // console.info(arguments);
       $('#endRange.datepicker').datepicker('option', 'minDate', dateStr);
     },
     onClose: function () {
-          // needed for mdl textfield floading labels!
+      // needed for mdl textfield floading labels!
       if ($(this).parent().get(0).MaterialTextfield) {
         $(this).parent().get(0).MaterialTextfield.checkDirty();
       }
@@ -87,31 +150,30 @@ $(document).ready(function () {
     changeMonth: true,
     changeYear: true,
     onSelect: function (dateStr) {
-          // console.info(arguments);
       $('#startRange.datepicker').datepicker('option', 'maxDate', dateStr);
     },
     onClose: function () {
-          // needed for mdl textfield floading labels!
+      // needed for mdl textfield floading labels!
       if ($(this).parent().get(0).MaterialTextfield) {
         $(this).parent().get(0).MaterialTextfield.checkDirty();
       }
     }
   });
 
-      // make modal draggable!
+  // make modal draggable!
   $('.modal-dialog').each(function () {
     $(this).draggable({
       handle: '.modal-header'
     });
   });
 
-      // init select components
+  // init select components
   $('.select2').select2({ allowEmptyOption: true });
   $('.select2-multiple').select2({ allowEmptyOption: true });
   $('.selectize').selectize({ allowClear: true });
   $('.selectize-multiple').selectize({ allowClear: true });
 
-      // Security Footers
+  // Security Footers
   var hulkIt = function (className) {
     $('footer.cbp-banner').removeClass().addClass('cbp-banner ' + className);
   };
